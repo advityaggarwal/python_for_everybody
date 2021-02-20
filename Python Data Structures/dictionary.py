985c1f3a420fb8da7414c2203d63e13147284b7b
advityaggarwal <advitya.aggarwal@outlook.com>
2021-02-21 03:01:04 +0530

    added tuple

--- Python Data Structures/dictionary.py
+++ Python Data Structures/dictionary.py
@@ -5,8 +5,7 @@ dic = dict();
 for line in handle:
     if line.startswith('From '):
         dic[line.split()[1]] = dic.get(line.split()[1],0) + 1;
-largestCount = None
-largestProlific = None
+(largestCount, largestProlific) = (None, None)
 #dictionaries are mutable
 #lists are mutable
 for key,value in dic.items():
@@ -14,3 +13,8 @@ for key,value in dic.items():
         largestProlific = key
         largestCount = value
 print(largestProlific, largestCount)
+
+#dictionary items are not of the same order as inserted
+#dict.keys() it gives the keys of dict in list format
+#dict.values() it gives the values of dict in list format
+#dict.items() it gives the key value pairs as items in list format that is each element of this list is a tuple in itself
